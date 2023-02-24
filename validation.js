bc68483f0b5627c9805f9b9d7642110a2ebb5deb
Josinaldo-Jr <josinaldoeuzebiojr@gmail.com>
2023-02-24 00:49:33 -0300

    Add regex and form validation

--- validation.js
+++ validation.js
@@ -1,1 +1,27 @@
 // validation script here
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    username:/^\w{5,12}$/,
+    telephone:/^\d{11}$/,
+    password:/^[\w@-]{8,20}$/,
+    slug:/^[a-z\d-]{8,20}$/
+};
+
+
+
+// validation function
+function validate(field, regex){
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    }else{
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', (e)=>{
+
+        validate(e.target, patterns[e.target.attributes.name.value])
+    });
+});
